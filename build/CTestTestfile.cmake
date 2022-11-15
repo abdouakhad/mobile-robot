452609394ab90303038cb86fabd135ffea7c16f0
abdouakhad <akhad0015@gmail.com>
2022-11-15 11:22:04 +0000

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/caytu/chapter3_ws/src
+# Build directory: /home/caytu/chapter3_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("robot_description")
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/caytu/chapter3_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/opt/ros/noetic/lib:/home/caytu/Firmware/build/px4_sitl_default/build_gazebo'
+export PWD='/home/caytu/chapter3_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/caytu/chapter3_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/caytu/chapter3_ws/src:$ROS_PACKAGE_PATH"
